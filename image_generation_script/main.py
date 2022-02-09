5db16d56b23cfa388d72ff1681dab3c7acaa1e54
Dimitrios Ieronymakis <dimitris.ieronymakis@gmail.com>
2022-02-09 11:49:20 +0100

    added base arguments

--- image_generation_script/main.py
+++ image_generation_script/main.py
@@ -4,20 +4,25 @@ def main():
     parser = argparse.ArgumentParser()
 
     parser.add_argument("-img", action="store", dest="img", type=str)
-    parser.add_argument("-os", action="store", dest="octave_steps", default=0, type=int)
-    parser.add_argument("-ss", action="store", dest="step_size", default=0.01, type=float)
-    # extra arguments for experimentation
-    parser.add_argument("-b", action="store", dest="bands", 
-                        help="Can be used to set the number of bands",
-                        type=int)
-    parser.add_argument("-p", action="store", dest="permutations", 
-                        help="Can be used to set the signature size",
-                         type=int)
-    parser.add_argument("-t", action="store", dest="threshold", 
-                        help="Can be used to set the threshold",
-                        type=float)
-    parser.add_argument("-v", action="store", dest="verbose", 
-                        help="Can be set to 1 to see debug messages on terminal",
-                        default=0, type=int)
+
+    parser.add_argument("-ss", action="store", dest="step_size", 
+                        help="Sets the step_size per iteration",
+                        default=0.01, type=float)
+
+    parser.add_argument("-or", action="store", dest="octaves_range", 
+                        help="Set octave range",
+                        default=(-1,4), type=tuple)
+
+    parser.add_argument("-os", action="store", dest="steps_per_octave",
+                        help="Sets the number of steps per octave",
+                        default=80, type=int)
+    
+    parser.add_argument("-osc", action="store", dest="octave_scale", 
+                        help="Set scaling for each octave step",
+                        default=1.2, type=float)
+
     args = parser.parse_args()
     print("arguments passed:",args)
+
+if __name__ == "__main__":
+    main()
