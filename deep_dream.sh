1d69556203a3e01fb9fbe771b08fe0872219f62f
Dimitrios Ieronymakis <dimitris.ieronymakis@gmail.com>
2022-04-09 22:07:10 +0200

    added lighthouse example

--- deep_dream.sh
+++ deep_dream.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 python main.py \
--img input/leiden_night.jpg \
+-img input/lighthouse.jpg \
+-img_out lighthouse \
 -img_max_dim 1024 \
--img_out leiden_night \
 -ts 512 \
 -ss 0.01 \
--or -3 2 \
+-or -3 3 \
 -os 60 \
--osc 1.25 \
--m inceptionResNet \
--ml 20 25
+-osc 1.3 \
+-m inceptionV3 \
+-ml 0 2
