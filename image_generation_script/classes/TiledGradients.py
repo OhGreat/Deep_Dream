d6dbfbd5cedf25789d130b9457387d61d60bea20
Dimitrios Ieronymakis <dimitris.ieronymakis@gmail.com>
2022-04-09 03:12:55 +0200

    to fix error with rolled img

---
+++ image_generation_script/classes/TiledGradients.py
@@ -0,0 +1,94 @@
+import tensorflow as tf
+from images_aux import *
+
+class TiledGradients(tf.Module):
+    def __init__(self, model):
+        self.model = model
+
+    @tf.function(
+        input_signature=(
+            tf.TensorSpec(shape=[None,None,3], dtype=tf.float32),
+            tf.TensorSpec(shape=[2], dtype=tf.int32),
+            tf.TensorSpec(shape=[], dtype=tf.int32),)
+    )
+    def __call__(self, img, img_size, tile_size=512):
+        shift, img_rolled = random_roll(img, tile_size)
+
+        # Initialize the image gradients to zero.
+        gradients = tf.zeros_like(img_rolled)
+        
+        # Skip the last tile, unless there's only one tile.
+        xs = tf.range(0, img_size[1], tile_size)[:-1]
+        if not tf.cast(len(xs), bool):
+            xs = tf.constant([0])
+        ys = tf.range(0, img_size[0], tile_size)[:-1]
+        if not tf.cast(len(ys), bool):
+            ys = tf.constant([0])
+
+        for x in xs:
+            for y in ys:
+                # Calculate the gradients for this tile.
+                with tf.GradientTape() as tape:
+                # This needs gradients relative to `img_rolled`.
+                # `GradientTape` only watches `tf.Variable`s by default.
+                    tape.watch(img_rolled)
+
+                    # Extract a tile out of the image.
+                    img_tile = img_rolled[y:y+tile_size, x:x+tile_size]
+                    loss = self.calc_loss(img_tile)
+                    print("loss:",loss)
+                    print("img rolled:", img_rolled.shape)
+
+                # Update the image gradients for this tile.
+                gradients = gradients + tape.gradient(loss, img_rolled)
+
+        # Undo the random shift applied to the image and its gradients.
+        gradients = tf.roll(gradients, shift=-shift, axis=[0,1])
+
+        # Normalize the gradients.
+        gradients /= tf.math.reduce_std(gradients) + 1e-8 
+        return gradients 
+
+    def calc_loss(self, img):
+        # Pass forward the image through the model to retrieve the activations.
+        # Converts the image into a batch of size 1.
+        img_batch = tf.expand_dims(img, axis=0)
+        layer_activations = self.model(img_batch)
+        if len(layer_activations) == 1:
+            layer_activations = [layer_activations]
+
+        losses = []
+        for act in layer_activations:
+            loss = tf.math.reduce_mean(act)
+            losses.append(loss)
+
+        return  tf.reduce_sum(losses)
+
+    def run_deep_dream_with_octaves(self, img, steps_per_octave=20, step_size=0.01, 
+                                octaves=range(-3,3), octave_scale=1.2):
+        base_shape = tf.shape(img)
+        print("base shape:", base_shape) # ok
+        #img = tf.keras.utils.img_to_array(img)
+        #img = tf.keras.applications.inception_v3.preprocess_input(img)
+        img = tf.keras.applications.inception_resnet_v2.preprocess_input(img)
+
+        initial_shape = img.shape[:-1]
+        img = tf.image.resize(img, initial_shape)
+        for octave in octaves:
+            # Scale the image based on the octave
+            new_size = tf.cast(tf.convert_to_tensor(base_shape[:-1]), tf.float32)*(octave_scale**octave)
+            new_size = tf.cast(new_size, tf.int32)
+            img = tf.image.resize(img, new_size)
+
+            for step in range(steps_per_octave):
+                gradients = self(img, new_size)
+                img = img + gradients*step_size
+                img = tf.clip_by_value(img, -1, 1)
+
+                if step % 10 == 0:
+                    display.clear_output(wait=True)
+                    show_np_img(deprocess(img), use_pil=True)
+                    print ("Octave {}, Step {}".format(octave, step))
+            
+        result = deprocess(img)
+        return result
--- image_generation_script/main.py
+++ image_generation_script/main.py
@@ -1,30 +1,82 @@
+import sys
+import numpy as np
 import argparse
+import pathlib
+import tensorflow as tf
+import IPython.display as display
+from classes.TiledGradients import *
+from images_aux import *
+
+def get_usable_layers(self):
+        """
+        Returns a list of all the usuable layers that we can feed as outputs for our model, 
+        together with the list of names
+        """
+        usable_layers = []
+        layer_names = []
+
+        for layer in self.layers:
+            if isinstance(layer, tf.keras.layers.Concatenate):
+                usable_layers.append(layer)
+                layer_names.append(layer.name)
+        return usable_layers, layer_names
 
 def main():
+    """ to add in parser:
+            - usable layers line 60
+            - img_to_np line 69
+            - random_roll size line 70
+            
+    """
     parser = argparse.ArgumentParser()
-
-    parser.add_argument("-img", action="store", dest="img", type=str)
-
+    parser.add_argument("-img", action="store", dest="img", type=str,
+                        default="image_generation_script/2022_01_23_17_11_23_0.png")
     parser.add_argument("-ss", action="store", dest="step_size", 
                         help="Sets the step_size per iteration",
                         default=0.01, type=float)
-
     parser.add_argument("-or", action="store", dest="octaves_range", 
-                        help="Set octave range",
-                        default=(-1,4), type=tuple)
-
+                        nargs="+", help="Set octave range",
+                        default=[-1,4], type=float)
     parser.add_argument("-os", action="store", dest="steps_per_octave",
                         help="Sets the number of steps per octave",
                         default=80, type=int)
-    
     parser.add_argument("-osc", action="store", dest="octave_scale", 
                         help="Set scaling for each octave step",
                         default=1.2, type=float)
-
+    parser.add_argument("-m", action="store", dest="model", 
+                        help="Set the model to use",
+                        default="inception_resnet", type=str)
     args = parser.parse_args()
     print("arguments passed:",args)
 
+    # Choose model
+    if args.model == 'inceptionV3':
+        base_model = tf.keras.applications.InceptionV3(include_top=False, weights='imagenet')
+    elif args.model == 'inceptionResNet':
+        base_model = tf.keras.applications.InceptionResNetV2(include_top=False, weights='imagenet')
+    else:
+        print('Please select a valid model.')
+        exit()
+    
+    # Pick model layers
+    layers, names = get_usable_layers(base_model)
+    layer_outputs = [layer.output for layer in layers]
+    layer_outputs = layer_outputs[25:30]
 
+    # Create model from selected layers
+    dream_model = tf.keras.Model(inputs=base_model.input, outputs=layer_outputs)
+    tiled_gradients = TiledGradients(dream_model)
+
+    # Preprocess input
+    original_img = img_to_np(args.img,max_dim=256)
+    print("input img shape:",original_img.shape)
+    img = tiled_gradients.run_deep_dream_with_octaves(img=original_img, steps_per_octave=80, step_size=0.01, 
+                                octaves=range(1,3), octave_scale=1.2)
+
+    display.clear_output(wait=True)
+    #img = tf.image.resize(img, base_shape)
+    #img = tf.image.convert_image_dtype(img/255.0, dtype=tf.uint8)
+    show_np_img(img,use_pil=True)
 
 if __name__ == "__main__":
     main()
