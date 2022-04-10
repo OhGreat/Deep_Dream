9cb9823f97790e32129a0623bdd1df1a6e789267
Dimitrios Ieronymakis <dimitris.ieronymakis@gmail.com>
2022-04-10 14:56:41 +0200

    added random noise images

--- deep_dream.sh
+++ deep_dream.sh
@@ -4,10 +4,10 @@ python deep_dream.py \
 -img random_noise \
 -img_out random_noise_3 \
 -img_max_dim 1024 \
--ts 256 \
+-ts 512 \
 -ss 0.01 \
 -or -4 2 \
--os 80 \
--osc 1.2 \
+-os 100 \
+-osc 1.3 \
 -m inceptionResNet \
 -ml 25 30
