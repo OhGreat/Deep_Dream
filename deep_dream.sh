bcb0cab9b974e5894fe391ce4ceb69e426b221f6
Dimitrios Ieronymakis <dimitris.ieronymakis@gmail.com>
2022-04-11 03:29:10 +0200

    added snow example to complete readme

--- deep_dream.sh
+++ deep_dream.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 python deep_dream.py \
--img input/input_reshaped/field_reshaped.jpg \
--img_out field_2 \
+-img input/snow.jpg \
+-img_out snow \
 -img_max_dim 1024 \
 -ts 600 \
 -ss 0.01 \
--or -3 3 \
+-or -4 3 \
 -os 50 \
 -osc 1.25 \
 -m inceptionResNet \
--ml 12 35
+-ml 10 30
