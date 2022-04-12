4678c8e96b776a3090d458844cebe880d5a60c7e
Dimitrios Ieronymakis <dimitris.ieronymakis@gmail.com>
2022-04-12 20:37:12 +0200

    edited gif + readme

--- deep_dream.sh
+++ deep_dream.sh
@@ -4,10 +4,10 @@ python deep_dream.py \
 -img readme_files/input/park_reshaped.jpeg \
 -img_out park_6 \
 -img_max_dim 1500 \
--ts 512 \
+-ts 500 \
 -ss 0.01 \
 -or -4 2 \
--os 50 \
--osc 1.1 \
+-os 30 \
+-osc 1.2 \
 -m inceptionResNet \
--ml 20 35
+-ml  0 8
