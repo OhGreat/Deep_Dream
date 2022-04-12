36647797c7285bde867d31e076faa24368cb848a
Dimitrios Ieronymakis <dimitris.ieronymakis@gmail.com>
2022-04-12 16:32:18 +0200

    created last img for banner

--- deep_dream.sh
+++ deep_dream.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 python deep_dream.py \
--img input/park.jpg \
+-img readme_files/input/park_reshaped.jpeg \
 -img_out park_6 \
 -img_max_dim 1500 \
 -ts 512 \
 -ss 0.01 \
--or -40 2 \
--os 5 \
--osc 1.02 \
+-or -4 2 \
+-os 50 \
+-osc 1.1 \
 -m inceptionResNet \
--ml 0 30
+-ml 20 35
